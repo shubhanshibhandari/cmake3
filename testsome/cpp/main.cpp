0145cb120e4b5d554208a70a50da3c2661a6f6cd
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-11-01 21:44:45 +0530

    Submodules

--- testsome/cpp/main.cpp
+++ testsome/cpp/main.cpp
@@ -1,6 +1,39 @@
 #include "adder.h"
 #include <iostream>
+#include <GLFW/glfw3.h>
 int main(){
     std::cout<< mearlymath::add(2,3);
+
+GLFWwindow* window;
+    // int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+     window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+     if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+     while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        // draw();
+
+        // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 } 
