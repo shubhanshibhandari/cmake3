06c39ce5f5da03f35f68937e2be28a36271e3fbe
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-11-01 16:08:55 +0530

    add submodule gflw

---
+++ somelibDemo/cpp/adder.cpp
@@ -0,0 +1,9 @@
+#include "adder.h"
+int mearlymath::add(int a, int b){
+	return(a+b);
+}
+
+float mearlymath::add(float a,float b){
+	return(a+b);
+}
+
---
+++ testsome/cpp/main.cpp
@@ -0,0 +1,6 @@
+#include "adder.h"
+#include <iostream>
+int main(){
+    std::cout<< mearlymath::add(2,3);
+    return 0;
+} 
